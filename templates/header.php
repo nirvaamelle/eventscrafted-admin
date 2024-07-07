eb8bcc53a20ef0d4583ab3c3c1b1d1823d959891
Arvin <arvin@devwerkz.com>
2024-07-08 01:12:53 +0800

    update

--- templates/header.php
+++ templates/header.php
@@ -31,7 +31,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											<div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="assets/images/avatars/ncst-2.png" class="msg-avatar" alt="user avatar">
+													<img src="assets/images/avatars/ncst-2.jpg" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">Daisy Anderson<span class="msg-time float-end">5 sec
@@ -53,7 +53,7 @@
 					</div>
 					<div class="user-box dropdown px-3">
 						<a class="d-flex align-items-center nav-link dropdown-toggle gap-3 dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							<img src="assets/images/avatars/ncst-1.png" class="user-img" alt="user avatar">
+							<img src="assets/images/avatars/ncst-1.jpg" class="user-img" alt="user avatar">
 							<div class="user-info">
 								<p class="user-name mb-0">Genefred De Guzman</p>
 								<p class="designattion mb-0">Administrator</p>
