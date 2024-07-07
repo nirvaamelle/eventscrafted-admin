113a8076c891de02b1b85f8deb71aac72b08d07a
Arvin <arvin@devwerkz.com>
2024-07-08 01:38:18 +0800

    updates

--- index.php
+++ index.php
@@ -59,8 +59,8 @@
 								<div class="d-flex align-items-center">
 									<div>
 										<p class="mb-0">Revenue</p>
-										<h4 class="my-1">$4805</h4>
-										<p class="mb-0 font-13"><i class='bx bxs-up-arrow align-middle'></i>$34 Since last week</p>
+										<h4 class="my-1">₱480,000</h4>
+										<p class="mb-0 font-13"><i class='bx bxs-up-arrow align-middle'></i>$80,000 Since last week</p>
 									</div>
 									<div class="widgets-icons ms-auto"><i class='bx bxs-wallet'></i>
 									</div>
@@ -75,8 +75,8 @@
 								<div class="d-flex align-items-center">
 									<div>
 										<p class="mb-0">Total Customers</p>
-										<h4 class="my-1">8.4K</h4>
-										<p class="mb-0 font-13"><i class='bx bxs-up-arrow align-middle'></i>14% Since last week</p>
+										<h4 class="my-1">6</h4>
+										<p class="mb-0 font-13"><i class='bx bxs-up-arrow align-middle'></i>20% Since last week</p>
 									</div>
 									<div class="widgets-icons ms-auto"><i class='bx bxs-group'></i>
 									</div>
@@ -91,8 +91,8 @@
 								<div class="d-flex align-items-center">
 									<div>
 										<p class="mb-0">Store Visitors</p>
-										<h4 class="my-1">59K</h4>
-										<p class="mb-0 font-13"><i class='bx bxs-down-arrow align-middle'></i>12.4% Since last week</p>
+										<h4 class="my-1">1k</h4>
+										<p class="mb-0 font-13"><i class='bx bxs-down-arrow align-middle'></i>15.1% Since last week</p>
 									</div>
 									<div class="widgets-icons ms-auto"><i class='bx bxs-binoculars'></i>
 									</div>
--- templates/side-nav.php
+++ templates/side-nav.php
@@ -5,10 +5,5 @@
         </div>
             <div class="menu-title">Dashboard</div>
         </a>
-        <a href="widgets.php">
-            <div class="parent-icon"><i class='bx bx-cookie'></i>
-            </div>
-            <div class="menu-title">Widgets</div>
-        </a>
     </li>
 </ul>
